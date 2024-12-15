e43008a7d4334eec96c68680325e73e90b7a97be
shaxrizodxayitboyev <fixredmi18@gmail.com>
2024-12-15 11:28:38 +0500

    changed to class apiview

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('blogs/', views.list_blogs, name='list-blogs'),
-    path('blog/<int:pk>/detail', views.detail_blog, name='detail-blog'),
+    # fbw
     path('blogs/search/', views.search_blogs, name='search-blogs'),
-    path('blogs/create/', views.blogs_create, name='blogs-create'),
-    path('blogs/update/<int:pk>', views.blogs_update, name='blogs_update'),
-    path('blogs/delete/<int:pk>', views.blogs_delete, name='delete-blogs'),
+
+    # cbw
+    path('cbw/blogs/', views.BlogListCreateAPIView.as_view(), name='cbw-blogs-list-create'),
+    path('cbw/blogs/<int:pk>/', views.BlogRetriveUpdateDetailAPIView.as_view(), name='cbw-blogs-delete-retrive-update')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,30 +2,46 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from django.db.models import Q
+from rest_framework.views import APIView
+
 from blog.models import Blog
 from blog.serializers import BlogSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])  # LIST
-def list_blogs(request):
-    # /api/v1/blogs/
-    blogs = Blog.objects.all()
-    serializer = BlogSerializer(blogs, many=True)
-    return Response(serializer.data)
+class BlogListCreateAPIView(APIView):
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])  # RETRIEVE
-def detail_blog(request, pk):
-    # /api/v1/blog/<int:pk>/detail
-    try:
-        blog = Blog.objects.get(id=pk)
-    except Blog.DoesNotExist:
-        data = {"detail": f"Blog {pk} is not found!"}
-        return Response(data)
+class BlogRetriveUpdateDetailAPIView(APIView):
+    def get(self, request, pk):
+        blog = get_object_or_404(Blog, id=pk)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
 
-    serializer = BlogSerializer(blog)
-    return Response(serializer.data)
+    def put(self, request, pk):
+        blog = get_object_or_404(Blog, id=pk)
+        serializer = BlogSerializer(Blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        blog = get_object_or_404(Blog, id=pk)
+        if blog:
+            blog.delete()
+            return Response({"success": "Blog deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET'])
@@ -45,35 +61,3 @@ def search_blogs(request):
 
     serializer = BlogSerializer(blogs, many=True)
     return Response(serializer.data)
-
-
-@api_view(['POST'])
-def blogs_create(request):
-    try:
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            blog = serializer.save()
-            response_serializer = BlogSerializer(blog)
-            return Response({"success": "Created OK", "detail": response_serializer.data}, status=201)
-        return Response(serializer.errors, status=400)
-    except Exception as e:
-        return Response({"errors": str(e)}, status=500)
-
-
-@api_view(['PUT'])
-def blogs_update(request, pk):
-    blog = get_object_or_404(Blog, id=pk)
-    serializer = BlogSerializer(blog, request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"detail": "Hammasi muvoffaqiyatli yaratildi."}, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-def blogs_delete(request, pk):
-    blog = get_object_or_404(Blog, id=pk)
-    if blog:
-        blog.delete()
-        return Response({"success": f"Blog id: {pk} muvaffaqiyatli o'chirildi"}, status=status.HTTP_204_NO_CONTENT)
-    return Response({"error": "Blog topilmadi!"}, status=status.HTTP_400_BAD_REQUEST)
